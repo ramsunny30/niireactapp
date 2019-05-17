626d75d3d5a2ac3e69708c6ee0f91854e647a7b1
maci <maci@macis-MacBook-Pro.local>
2019-05-17 08:18:36 -0400

    https://github.com/ramsunny30/niireact.git

--- src/Blog.js
+++ src/Blog.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Blog=()=> {
     return (
         <div>
-          <h1>"Wealth is created in concentration and preserved in diversification.”</h1>
+          <h1>"Wealth is created in concentration and preserved in diversification”</h1>
         </div>
     )
 }
