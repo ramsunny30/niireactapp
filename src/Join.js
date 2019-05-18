b218ea822ef24c47dea609d4e7504c7857bb21e5
ramsunny <ramsunny30@gmail.com>
2019-05-17 22:54:48 -0400

    commit

--- src/Join.js
+++ src/Join.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
+import "./App.css";
+import FacebookLogin from 'react-facebook-login';
+import TwitterLogin from 'react-twitter-auth';
 
 export const Join=()=> {
     return (
-        <div>
-          <h1>Your in Join Page</h1>
-        </div>
+      <div className="wrapper">
+      <div className="form-wrapper1">
+        <h1>Signup</h1>
+            <div className="firstName">
+              <label htmlFor="firstName">First Name</label>
+              <input
+                placeholder="Your First Name"
+                type="text"
+                name="firstName"
+                noValidate
+              />
+            </div>
+            <div className="lastName">
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                placeholder="Your Last Name"
+                type="text"
+                name="firstName"
+                noValidate
+              />
+            </div>
+          
+          <div className="email">
+            <label htmlFor="email">Email ID</label>
+            <input
+              placeholder="Email"
+              type="email"
+              name="email"
+              noValidate
+            />
+            </div>
+            <div className="password">
+            <label htmlFor="password">Password</label>
+            <input
+              placeholder="Password (min 8 characters)"
+              type="password"
+              name="password"
+              noValidate
+            />
+            </div>
+            <div className="createAccount">
+              <button type="submit">Signup</button>
+              <div className="createAccount">
+              <small>By signing up, I agree to NII's Terms of Service</small>
+              <small>Already have an account? <a href="http://localhost:3000/Login">Log in »</a> </small>
+            </div>
+            </div>
+            <div id="fb-root"></div>
+
+            <br></br> 
+            <br></br>
+            <TwitterLogin
+            loginUrl="http://localhost:4000/api/v1/auth/twitter"
+            requestTokenUrl="http://localhost:4000/api/v1/auth/twitter/reverse"
+             />
+            
+      </div>
+    </div>
+  
     )
 }
--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,34 @@
-import React from 'react'
-
+import React, { Component } from "react";
+import "./App.css";
 export const Login=()=> {
     return (
-        <div>
-          <h1>Your in Login Page</h1>
-        </div>
+      <div className="wrapper">
+      <div className="form-wrapper">
+        <h1>Login</h1>
+          <div className="email">
+            <label htmlFor="email">User ID</label>
+            <input
+              placeholder="Email"
+              type="email"
+              name="email"
+              noValidate
+            />
+            </div>
+            <div className="password">
+            <label htmlFor="password">Password</label>
+            <input
+              placeholder="Password"
+              type="password"
+              name="password"
+              noValidate
+            />
+            </div>
+            <div className="createAccount">
+              <button type="submit">Signin</button>
+              <small><a href="http://localhost:3000/Join">Create a new Account?</a></small>
+            </div>
+      </div>
+    </div>
+  
     )
 }
