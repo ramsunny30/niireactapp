a3b440b3b2c8df5b4675ca3ce068bdf6d7382400
ramsunny <ramsunny30@gmail.com>
2019-05-22 23:51:14 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+const rootEl = document.getElementById('root');
+
+let render = () => {
+    ReactDOM.render(<App />,rootEl)
+}
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        setTimeout(render);
+    })
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
